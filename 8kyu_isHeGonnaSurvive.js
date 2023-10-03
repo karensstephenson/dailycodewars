7fa970c0ccb5b8dd41ef891c51fe686a57ed96bf
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-10-03 09:27:35 +0100

    codewars 03/10/23

---
+++ 8kyu_isHeGonnaSurvive.js
@@ -0,0 +1,22 @@
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+// Return true if yes, false otherwise :)
+
+function hero(bullets, dragons){
+    if (bullets >= dragons * 2) {
+        return true
+    } else{
+      return false  
+    }
+    
+    }
+
+// shorter alternative
+function hero2(bullets, dragons) {
+    return bullets >= dragons * 2
+}
+
+// same using a ternary
+function hero3(bullets, dragons) {
+    return (bullets >= dragons * 2) ? true : false
+}
