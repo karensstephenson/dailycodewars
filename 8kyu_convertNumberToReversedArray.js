c05a7383b04430812a1c92654a3749ef08ea13ba
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-10-08 16:51:32 +0100

    codewars 08/10/23

---
+++ 8kyu_convertNumberToReversedArray.js
@@ -0,0 +1,17 @@
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+// Example(Input => Output):
+
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+function digitize(n) {
+    return n.toString().split('').reverse().map(x => Number(x))
+  }
+
+// Alternative with shorter map()
+function digitize2(n) {
+    return n.toString().split('').reverse().map(Number)
+    
+  }
+
+console.log(digitize(123456))
