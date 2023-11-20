c7db4c95b77a476c3036a19922b67518fe8dc74d
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-11-20 10:24:07 +0000

    codewars 20/11/23

---
+++ 7kyu_find_the_capitals.py
@@ -0,0 +1,19 @@
+# Instructions
+
+# Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+# Example (Input --> Output)
+
+# "CodEWaRs" --> [0,3,4,6]
+
+def capitals(word):
+    caps = []
+    for x in range(len(word)):
+        if 'A' <= word[x] <= 'Z':
+            caps.append(x)
+    return caps
+
+print(capitals("CodEWaRs"))
+
+# alternative using isupper()
+def capitals2(word):
+    return [i for i in range(len(word) if word[i].isupper())]
