39ec2f32457e60142ea4c55758054ba79f1f17fe
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-12-01 10:24:16 +0000

    codewars 01/12/23

--- 7kyu_no_oddities_here.py
+++ 7kyu_no_oddities_here.py
@@ -3,6 +3,6 @@
 # All values in the array will be integers. Return the good values in the order they are given.
 
 def no_odds(values):
-    return [i for i in values if i%2 == 0]
+    return [i for i in values if i%2 == 0 ]
 
 print(no_odds([1, 3, 4, 5, 7, 9]))
