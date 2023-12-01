b64c513151227e5325985f0394e4ac898c28fab7
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-12-01 09:45:15 +0000

    codewars 1/12/23

---
+++ 7kyu_no_oddities_here.py
@@ -0,0 +1,8 @@
+# Write a small function that returns the values of an array that are not odd.
+
+# All values in the array will be integers. Return the good values in the order they are given.
+
+def no_odds(values):
+    return [i for i in values if i%2 == 0]
+
+print(no_odds([1, 3, 4, 5, 7, 9]))
